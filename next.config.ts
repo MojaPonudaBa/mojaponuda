7f8317c56a20b54acc0f315cdff314072e16387c
KriptonDude <260524457+IMakeFreeApps@users.noreply.github.com>
2026-03-12 13:45:05 +0100

    fix: revert to clean config - middleware already deleted, config changes caused 404

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.node = {
-        ...config.node,
-        __dirname: true,
-      };
-    }
-    return config;
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
